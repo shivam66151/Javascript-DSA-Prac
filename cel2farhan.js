5953ad7246237b6ba71f96c0c23d275b2a130f6b
shivam66151 <sharmashivam66151@gmail.com>
2022-03-19 18:56:38 +0530

    1 file commited

--- cel2farhan.js
+++ cel2farhan.js
@@ -1,8 +1,8 @@
+//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
 function convertToF(celsius) {
     let fahrenheit = celsius * (9 / 5) + 32;
     return fahrenheit;
   }
   
 console.log(convertToF(30))
-
-//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
