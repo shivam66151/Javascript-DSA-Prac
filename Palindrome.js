6b60f08e3f2be60cd72e7e13c4e87b9a638a634c
shivam66151 <sharmashivam66151@gmail.com>
2022-03-20 23:23:11 +0530

    1 file commited

--- Palindrome.js
+++ Palindrome.js
@@ -1,15 +1,25 @@
-function palindrome(str) {
-    const alphanumericOnly = str
-        // 1) Lowercase the input
-        .toLowerCase()
-        // 2) Strip out non-alphanumeric characters
-        .match(/[a-z0-9]/g);
+// function palindrome(str) {
+//     const alphanumericOnly = str
+//         // 1) Lowercase the input
+//         .toLowerCase()
+//         // 2) Strip out non-alphanumeric characters
+//         .match(/[a-z0-9]/g);
         
-    // 3) return string === reversedString
-    return alphanumericOnly.join('') ===
-        alphanumericOnly.reverse().join('');
+//     // 3) return string === reversedString
+//     return alphanumericOnly.join('') ===
+//         alphanumericOnly.reverse().join('');
+// }
+
+
+
+// palindrome("eye");
+
+
+//Find whether a string contains a contiguous palindromic substring in O(n) time. Can you solve the problem in O(1) time?
+
+
+function checkPalindrom(str) {
+    return str == str.split('').reverse().join('');
 }
 
-
-
-palindrome("eye");
+console.log(checkPalindrom("eye"))
