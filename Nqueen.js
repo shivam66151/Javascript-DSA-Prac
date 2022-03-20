e5dd3691daf6b5ee3e7c866734bb974101323ae7
shivam66151 <sharmashivam66151@gmail.com>
2022-03-20 22:37:17 +0530

    1 file commited

--- Nqueen.js
+++ Nqueen.js
@@ -57,3 +57,60 @@ function isSafe(board, row, col)
  
     return true
 }
+
+function solveNQUtil(board, col){
+     
+    // base case: If all queens are placed
+    // then return true
+    if(col >= N)
+        return true
+ 
+    // Consider this column and try placing
+    // this queen in all rows one by one
+    for(let i=0;i<N;i++){
+ 
+        if(isSafe(board, i, col)==true){
+             
+            // Place this queen in board[i][col]
+            board[i][col] = 1
+ 
+            // recur to place rest of the queens
+            if(solveNQUtil(board, col + 1) == true)
+                return true
+ 
+            // If placing queen in board[i][col
+            // doesn't lead to a solution, then
+            // queen from board[i][col]
+            board[i][col] = 0
+        }
+    }
+    // if the queen can not be placed in any row in
+    // this column col then return false
+    return false
+}
+ 
+// This function solves the N Queen problem using
+// Backtracking. It mainly uses solveNQUtil() to
+// solve the problem. It returns false if queens
+// cannot be placed, otherwise return true and
+// placement of queens in the form of 1s.
+// note that there may be more than one
+// solutions, this function prints one of the
+// feasible solutions.
+function solveNQ(){
+    let board = [ [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0] ]
+ 
+    if(solveNQUtil(board, 0) == false){
+        document.write("Solution does not exist")
+        return false
+    }
+ 
+    printSolution(board)
+    return true
+}
+ 
+// Driver Code
+solveNQ()
