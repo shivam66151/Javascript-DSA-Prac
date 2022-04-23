3390a6900c87a91823dd19d9e361ab38623696e2
shivam66151 <sharmashivam66151@gmail.com>
2022-04-23 10:54:29 +0530

    1 file commit

--- checksumzero.js
+++ checksumzero.js
@@ -1,4 +1,20 @@
-//Checking sum zero --Problem 1
+//Checking sum zero -- Problem 1
 //[-5,-4,-3,-2,0,2,4,6,8] => input
 // [?,?] => output
 
+function SumZeroPair(arr) {
+    for(let number of arr) {
+        console.log("Outer loop traversing")
+        for(let j = 1; j < arr.length; j++) {
+            console.log("Inner loop traversing")
+            if(number + arr[j] === 0) {
+                return [number, arr[j]]
+            }
+        }
+    }
+}
+
+const result = SumZeroPair([-5,-4,-3,-2,0,2,4,6,8])
+console.log(result)
+
+// O(n^2)  quadratic time complexity
---
+++ sumzerooptimized.js
@@ -0,0 +1,13 @@
+//Checking sum zero -- Problem 1
+//[-5,-4,-3,-2,0,2,4,6,8] => input
+// [?,?] => output
+
+
+// Optimized solution
+
+function SumZeroPair(arr) {
+
+}
+
+SumZeroPair([-5,-4,-3,-2,0,2,4,6,8])
+
