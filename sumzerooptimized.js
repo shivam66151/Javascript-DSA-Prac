d324ba9bb6f55f7cd82dbf0e8e8a7db20751e328
shivam66151 <sharmashivam66151@gmail.com>
2022-04-23 22:21:48 +0530

    1 file commit

--- sumzerooptimized.js
+++ sumzerooptimized.js
@@ -5,9 +5,41 @@
 
 // Optimized solution
 
-function SumZeroPair(arr) {
+// function SumZeroPair(arr) {
+//  let left = 0;
+//  let right = arr.length - 1;
+ 
+//  while(left < right) {
+//      sum = arr[left] + arr[right];
+//      if(sum===0) {
+//          return [arr[left],arr[right]]
+//      } else if(sum > 0) {
+//          right--
+//      } else {
+//          left ++
+//      }
+//  }
+// }
 
+// let result = SumZeroPair([-5,-4,-3,-2,0,2,4,6,8])
+// console.log(result)
+
+
+// if arr is not sorted
+
+let arr = [-5,-3,-1,0,2,4,6,8,-4]
+
+function findPair(arr) {
+    let pair = [];
+    for (let i =0;i < arr.length;i++) {
+        let x = -arr[i]
+        if(pair[x]) {
+            return [x,arr[i]];
+        }
+        pair[arr[i]] = 1
+    }
+    return [];
 }
 
-SumZeroPair([-5,-4,-3,-2,0,2,4,6,8])
-
+const pair = findPair(arr)
+console.log(pair)
