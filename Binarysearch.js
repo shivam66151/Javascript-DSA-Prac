d47a18a33c82dd7d7804ae45dc4581ca5864f124
shivam66151 <sharmashivam66151@gmail.com>
2022-05-02 11:05:11 +0530

    1 file commit

---
+++ Binarysearch.js
@@ -0,0 +1,7 @@
+// Divide and Conquer -> Binary Search
+// FInd the Index of given number in a sorted array 7
+// [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] => index 6 -> output
+
+function binsearch() {
+    
+}
--- countunique.js
+++ countunique.js
@@ -5,20 +5,24 @@
 // Output => 8
 
 function countUnique(arr) {
-    if(arr.length > 0) {
+    if(arr.length > 0) 
+    {
         let i = 0;
-        for(let j = 1; j < arr.length; j++) {
-            if(arr[i]!==arr[j])  {
+        for(let j = 1; j < arr.length; j++) 
+        {
+            if(arr[i]!==arr[j])  
+            {
                 i++;
                 arr[i] = arr[j]
             }
-        } return i + 1
-    } else {
+        } 
+        return i + 1
+    } 
+    else 
+    {
         throw new Error("Array is empty");
     }
-
 }
-
 const result = countUnique([1,1,2,2,3,4,4,5,6,7,8,8,9,9,9])
 console.log(result)
 
