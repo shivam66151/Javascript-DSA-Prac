c2b44ad58a30e0857a9dbc47c16e01cfe9006a94
shivam66151 <sharmashivam66151@gmail.com>
2022-05-07 12:45:58 +0530

    minor change

--- checkSquareoptimized.js
+++ checkSquareoptimized.js
@@ -1,4 +1,7 @@
-// Optimized method of check square problem
-// arr1 = [1,2,3,4], arr2 = [1,9,4,16]
+// Optimized method of check square in another array problem
+// arr1 = [1,2,3,4], arr2 = [1,9,4,16] -> Case1
+// arr1 = [1,2,4,2] , arr2=[1,4,4,16] -> Case2
 
 // Target -> Linear time complexity
+
+//  
