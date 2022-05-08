2f198a627fc761bd2fc20527a302b785d4270743
shivam66151 <sharmashivam66151@gmail.com>
2022-05-08 21:57:07 +0530

    check square optimized

--- checkSquareoptimized.js
+++ checkSquareoptimized.js
@@ -4,4 +4,17 @@
 
 // Target -> Linear time complexity
 
-//  
+//  map1 = {1:1,2:2,4:1}
+//  map2 = {1:1,4:2,16:1}
+
+function checkSquare(array1,array2) {
+    let map1={}
+    let map2={}
+
+    for(item of array1) {
+
+    }
+    for(item2 of array2) {
+        
+    }
+}
