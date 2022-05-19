d1c2472f4bc2d98b5b91c9157cd4290bb03c3fbf
shivam66151 <sharmashivam66151@gmail.com>
2022-05-19 23:17:13 +0530

    Initial commit

---
+++ Sorting Recursion/merge.js
@@ -0,0 +1,9 @@
+// MErge sort usig DIvide and conquerer
+// main  concept of merge sort is that an array of length 1 is sorted
+// There fore the task lies in splitting the array into subarrays of size 1 and then merge them 
+
+// 1. split the array elements into individual elems
+// 2. compare the individual elems and arrange them order. This results in subarrays 
+// of length 1 or 2.
+// make an empty array
+// 
