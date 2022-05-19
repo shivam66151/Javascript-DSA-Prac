8be980e75d5f8bdae3cf5d453ae5a5bbb7542830
shivam66151 <sharmashivam66151@gmail.com>
2022-05-19 23:04:48 +0530

    Initial commit

---
+++ Sorting Recursion/selection.js
@@ -0,0 +1,17 @@
+// Selection sort using ascending order
+
+// arr = [29, 72, 98, 13, 87, 66, 52, 51, 36]
+
+// 1.Assume that the first element in the array is the smallest
+// 2.From the other portion of the array,find the minimum value and swap it with first element
+// First pass completed
+// Repeat same procedure with rest of the array comparing the elements to the right,not the left
+
+function selectionsort(arr) {
+    let min;
+
+    //start passes
+    for (let i = 0;i < arr.length;i++) {
+        // index of the smallest element to be the ith element
+    }
+}
