53fa4fbae63accd0c7e39c582e32fc997ddef75c
shivam66151 <sharmashivam66151@gmail.com>
2022-05-29 20:54:47 +0530

    commit

---
+++ Searching/linear.js
@@ -0,0 +1,7 @@
+// Linear search algorithm 
+
+const users = [
+    {username: "sharmashivam",email: "shivam66151@gmail.com"},
+    {username: "sharmashreya", email: "shreya@gmail.com"},
+    {username: "sangeetashaarma", email: "sangeeta@gmail.com"},
+]
